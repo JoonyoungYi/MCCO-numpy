fad1c2f249597ae0b0b15cd186a94390eb4a45ff
Joonyoung Yi <joonyoung.yi@kaist.ac.kr>
2018-04-24 18:55:45 +0900

    bug fix(symmetric contraint) and more information on console

--- README.md
+++ README.md
@@ -2,12 +2,28 @@
 
 - This repository reproduce experiments in the paper, [Exact Matrix Completion via Convex Optimization](https://statweb.stanford.edu/~candes/papers/MatrixCompletion.pdf).
   - Candès, Emmanuel J., and Benjamin Recht. "Exact matrix completion via convex optimization." Foundations of Computational mathematics 9.6 (2009): 717.
+- I try to minimize the number of packages to install. As the result of this effort, you can use test this package by only installing several packages with python virtual environment.
 - How to install and run.
 ```
 virtualenv .venv -p python3
 . .venv/bin/activate
 pip install -r requirements.txt
 python run.py
+```
+- The example of result(console)
+```
+#row of M    : 20
+#column of M : 20
+RANK of M    : 2
+#sample      : 320
+NUC-NORM of M: 36.76954064144393
+STATUS       : optimal
+OPTIMAL VALUE: 73.5256314253263
+RANK of X    : 20
+NUC-NORM of X: 36.76281571725943
+TRAIN RMSE   : 6.626737339013724e-18
+TEST  RMSE   : 0.006495179200041315
+|X-M|_F/|M|_F: 0.019961863829646286
 ```
 - I implemented solution of convex optimization(nuclear norm minimization) via SDP(semidefinite programming).
 - The idea of process reducing from nuclear norm minimization to SDP is from \[Fazel et al. 2002\](The paper already mentioned).
@@ -17,6 +33,10 @@ n_1: The number of row of the matrix
 n_2: The number of column of the matrix
 r: The rank of the matrix.
 M: The matrix to recover.
-X': The matrix to recover in SDP.
+M_: Input of algorithm.
+X_: The matrix to recover in SDP.
 X: The solution of nuclear norm minimization.
+omega: The sample set. observed entry (i, j) in the sample set omega.
 ```
+- You can optimize more to enhance performance.
+- This paper handles the noise-less case. If your data has noise, you should add the regularizer term to objective function. See [cvxpy](http://www.cvxpy.org/en/latest/).
--- run.py
+++ run.py
@@ -1,49 +1,89 @@
 import random
+import math
+import itertools
 
 import numpy as np
 from cvxpy import *
 
 
-def solve(M, sampled_indices):
+def _solve(M, omega):
     n_1 = M.shape[0]
     n_2 = M.shape[1]
     X_ = Semidef(n_1 + n_2)
     objective = Minimize(trace(X_))
 
-    constraints = []
-    for i, j in sampled_indices:
+    constraints = [(X_ == X_.T)]  # add symmetric constraint.
+    for i, j in omega:
         constr = (X_[i, j + n_1] == M[i, j])
         constraints.append(constr)
     problem = Problem(objective, constraints)
     problem.solve(solver=CVXOPT)
 
+    print("STATUS       :", problem.status)
+    print("OPTIMAL VALUE:", problem.value)
+
     return X_.value[:n_1, n_1:]
 
 
+def _generate_omega(n_1, n_2, m):
+    return random.sample([(i, j) for i in range(n_1) for j in range(n_2)], m)
+
+
+def _get_mask_matrix(n_1, n_2, omega):
+    """
+    If we observed entry (i, j) of matrix M, the entry of mask matrix is 1,
+    Otherwise 0.
+    """
+    mask = np.zeros((n_1, n_2), dtype=np.int8)
+    for i, j in omega:
+        mask[i, j] = 1
+    return mask
+
+
+def _get_abs_max_from_matrix(M):
+    return np.max(np.absolute(M))
+
+
 def main(n_1, n_2, r, m):
+    print("#row of M    :", n_1)
+    print("#column of M :", n_2)
+    print("RANK of M    :", r)
+    print("#sample      :", m)
     L = np.random.randn(n_1, r)
     R = np.random.randn(n_2, r)
     M = np.dot(L, R.T)
-    M_max = np.max(M)
-    M_min = np.min(M)
+    M_abs_max = _get_abs_max_from_matrix(M)
+    # print("RANK of M    :", np.linalg.matrix_rank(M))
+    print("NUC-NORM of M:", np.linalg.norm(M, "nuc"))
 
-    all_indices = []
-    for i in range(n_1):
-        for j in range(n_2):
-            all_indices.append((i, j))
-    sampled_indices = random.sample(all_indices, m)
+    omega = _generate_omega(n_1, n_2, m)
+    mask = _get_mask_matrix(n_1, n_2, omega)
 
+    # M_ is for training data removing the data.
+    # This block should not affect the result.
+    # Just for defensive programming.
+    # M_ = np.random.uniform(-M_abs_max, M_abs_max, M.shape)
     M_ = M.copy()
-    # For masking. This loop should not affect the result.
-    for i in range(n_1):
-        for j in range(n_2):
-            if (i, j) not in sampled_indices:
-                M_[i, j] = M_max * M_max
-    X = solve(M_, sampled_indices)
+    np.place(M_, 1 - mask, M_abs_max * M_abs_max)
+    X = _solve(M_, omega)
+    X_rank = np.linalg.matrix_rank(X)
+    print("RANK of X    :", X_rank)
+    print("NUC-NORM of X:", np.linalg.norm(X, "nuc"))
 
-    print("ERROR:", (np.linalg.norm(X - M, "fro") / np.linalg.norm(M, "fro")))
+    E = np.subtract(M, X)
+    E_train = E.copy()
+    np.place(E_train, 1 - mask, 0)
+    print('TRAIN RMSE   :', np.linalg.norm(E_train, "fro") / m)
+    E_test = E.copy()
+    np.place(E_test, mask, 0)
+    print('TEST  RMSE   :', np.linalg.norm(E_test, "fro") / (n_1 * n_2 - m))
+
+    frobenius_norm_ratio = (
+        np.linalg.norm(X - M, "fro") / np.linalg.norm(M, "fro"))
+    print("|X-M|_F/|M|_F:", frobenius_norm_ratio)  # the metric of paper.
 
 
 if __name__ == '__main__':
     n = 20
-    main(n, n, r=2, m=380)
+    p = 0.8
+    main(n, n, r=2, m=int(n * n * p))
