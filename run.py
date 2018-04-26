a0a44451c8a14e342e331f2f2972f2affaf9ddbf
Joonyoung Yi <joonyoung.yi@kaist.ac.kr>
2018-04-26 16:45:42 +0900

    double check code add

--- run.py
+++ run.py
@@ -22,6 +22,9 @@ def _solve(M, omega):
     print("STATUS       :", problem.status)
     print("OPTIMAL VALUE:", problem.value)
 
+    X0 = X_.value
+    # check optimizer's solution is symmetric.
+    print("|X0-X0.T|_F  :", np.linalg.norm(np.subtract(X0, X0.T), "fro"))
     return X_.value[:n_1, n_1:]
 
 
@@ -47,14 +50,17 @@ def _get_abs_max_from_matrix(M):
 def main(n_1, n_2, r, m):
     print("#row of M    :", n_1)
     print("#column of M :", n_2)
-    print("RANK of M    :", r)
     print("#sample      :", m)
     L = np.random.randn(n_1, r)
     R = np.random.randn(n_2, r)
     M = np.dot(L, R.T)
     M_abs_max = _get_abs_max_from_matrix(M)
     # print("RANK of M    :", np.linalg.matrix_rank(M))
-    print("NUC-NORM of M:", np.linalg.norm(M, "nuc"))
+    print("|M|_*        :", np.linalg.norm(M, "nuc"))
+    M_rank = np.linalg.matrix_rank(M)
+    print("RANK of M    :", M_rank)
+    U, S, V_T = np.linalg.svd(M, full_matrices=False)
+    print(S)
 
     omega = _generate_omega(n_1, n_2, m)
     mask = _get_mask_matrix(n_1, n_2, omega)
@@ -68,7 +74,9 @@ def main(n_1, n_2, r, m):
     X = _solve(M_, omega)
     X_rank = np.linalg.matrix_rank(X)
     print("RANK of X    :", X_rank)
-    print("NUC-NORM of X:", np.linalg.norm(X, "nuc"))
+    print("|X|_*        :", np.linalg.norm(X, "nuc"))
+    U, S, V_T = np.linalg.svd(X, full_matrices=False)
+    print(S)
 
     E = np.subtract(M, X)
     E_train = E.copy()
